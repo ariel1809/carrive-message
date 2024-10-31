3d179d76a871e2395d5e84bfb6cb2e5f3e1103e5
ariel <Naruto18@>
2024-10-31 12:50:59 +0100

    message

--- src/main/java/com/example/carrivemessage/CarriveMessageApplication.java
+++ src/main/java/com/example/carrivemessage/CarriveMessageApplication.java
@@ -2,8 +2,12 @@ package com.example.carrivemessage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = {"com.manage.carriveutility.repository"})
+@EntityScan("com.manage.carrive")
 public class CarriveMessageApplication {
 
     public static void main(String[] args) {
